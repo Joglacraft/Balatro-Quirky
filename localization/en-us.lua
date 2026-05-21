0a902de2d6713c52c523ac30441f88206e1f1cfc
Evgast <136436468+Evgast@users.noreply.github.com>
2026-05-21 18:11:11 +1100

    Add files via upload

---
+++ localization/en-us.lua
@@ -0,0 +1,36 @@
+return {
+    descriptions = {
+            Quirky = {
+                j_joker = {
+                    name = "",
+                    text = {
+                        "Hello, I'm {C:attention}Jimbo!{} And I will show",
+                        "you around the world of {C:attention}Balatro!{}"
+                    }
+                },
+                j_ceremonial = {
+                    name = "",
+                    text = {
+                        "Copies ability of",
+                        "{C:attention}Joker{} to the right"
+                    }
+                },
+                j_stencil = {
+                    name = "",
+                    text = {""}
+                },
+                j_hanging_chad = {
+                    name = "",
+                    text = {
+                        "This isn't my {C:attention}first{} time"
+                    }
+                },
+                j_caino = {
+                    name = "",
+                    text = {
+                        "Off with their {C:attention}heads!{}"
+                    }
+                },
+            },
+    }
+}
---
+++ main.lua
@@ -0,0 +1,23 @@
+QRK = SMODS.current_mod
+
+QRK.calculate = function(self, context)
+	for k, v in pairs(G.jokers.cards) do
+		local key = v.config.center.key
+		if G.localization.descriptions.Quirky[key] then
+			if context.end_of_round and context.main_eval then
+				if not QRK.table_contains(G.PROFILES[G.SETTINGS.profile].dequirk, key) then
+					G.PROFILES[G.SETTINGS.profile].dequirk[#G.PROFILES[G.SETTINGS.profile].dequirk+1] = key
+				end
+			end
+		end
+	end
+end
+
+function QRK.table_contains(table, element) 
+     for _, value in pairs(table) do
+         if value == element then
+            return true
+        end
+    end
+    return false
+end
