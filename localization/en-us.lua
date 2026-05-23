eee3b93db81e66e0765975f93bdd5e53bab316b0
Evgast <polysap12@gmail.com>
2026-05-23 22:47:43 +1100

    Omg I didn't commit this shit yet? Whoops

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -31,6 +31,55 @@ return {
                         "Off with their {C:attention}heads!{}"
                     }
                 },
+                j_greedy_joker = {
+                    name = "",
+                    text = {
+                        "Hello, can you give me",
+                        "your {C:diamonds}Money{}, please?"
+                    }
+                },
+                j_lusty_joker = {
+                    name = "",
+                    text = {
+                        "Hello, can you give me",
+                        "your {C:hearts}Love{}, please?"
+                    }
+                },
+                j_wrathful_joker = {
+                    name = "",
+                    text = {
+                        "Hello.",
+                        "Go to {C:spades}Hell{}."
+                    }
+                },
+                j_gluttenous_joker = {
+                    name = "",
+                    text = {
+                        "Hello? You can still",
+                        "{C:clubs}feed{} me, I'm not done"
+                    }
+                },
+                j_jolly = {
+                    name = "",
+                    text = {
+                        "Also try {C:attention}Cryptid 2!{}",
+                        "...Not out yet? Oh..."
+                    }
+                },
+                j_lucky_cat = {
+                    name = "",
+                    text = {
+                        "Meow :3",
+                        "Mrow purr {C:green}'Lucky'{mm} mrau"
+                    }
+                },
+                j_square={
+                    name="Square Joker",
+                    text={
+                        "Be there, or be {C:attention}square!",
+                        "..Right."
+                    },
+                },
             },
     }
 }
--- main.lua
+++ main.lua
@@ -1,11 +1,35 @@
 QRK = SMODS.current_mod
 
+QRK.config_tab = function()
+    return {
+        n = G.UIT.ROOT,
+        config = { r = 0.1, minw = 8, align = "tm", padding = 0.2, colour = G.C.BLACK },
+        nodes = {
+            {n = G.UIT.R, config = { padding = 0.2 }, nodes = {
+                {n = G.UIT.C, config = { align = "cm" }, nodes = {
+                    {n = G.UIT.R, config = { align = "cm", padding = 0.01 }, nodes = {
+                        create_toggle({ label = "I don't want to know the effects", ref_table = QRK.config, ref_value = 'vagueslop_mode' })
+                    }},
+					{ n = G.UIT.R, config = { align = "cm", padding = 0.01 }, nodes = {
+                       {n = G.UIT.T, config = {text = "Jokers keep their quips instead of telling the info you need, duh", colour = G.C.UI.TEXT_LIGHT, scale = 0.3}}
+                    }},
+                }},
+            }},
+        }
+    }
+end
+
+
 QRK.calculate = function(self, context)
 	for k, v in pairs(G.jokers.cards) do
 		local key = v.config.center.key
-		if G.localization.descriptions.Quirky[key] then
-			if context.end_of_round and context.main_eval then
-				G.PROFILES[G.SETTINGS.profile].dequirk[key] = true
+		if not QRK.config.vagueslop_mode then
+			if G.localization.descriptions.Quirky[key] then
+				if context.end_of_round and context.main_eval then
+					if not QRK.table_contains(G.PROFILES[G.SETTINGS.profile].dequirk, key) then
+						G.PROFILES[G.SETTINGS.profile].dequirk[#G.PROFILES[G.SETTINGS.profile].dequirk+1] = key
+					end
+				end
 			end
 		end
 	end
