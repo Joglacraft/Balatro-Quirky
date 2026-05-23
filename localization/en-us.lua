f83cacebd5a157223cf21d1a7af77405915fe622
Evgast <polysap12@gmail.com>
2026-05-24 02:15:32 +1100

    No one escapes...

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -2,84 +2,137 @@ return {
     descriptions = {
             Quirky = {
                 j_joker = {
-                    name = "",
                     text = {
                         "Hello, I'm {C:attention}Jimbo!{} And I will show",
                         "you around the world of {C:attention}Balatro!{}"
                     }
                 },
                 j_ceremonial = {
-                    name = "",
                     text = {
                         "Copies ability of",
                         "{C:attention}Joker{} to the right"
                     }
                 },
                 j_stencil = {
-                    name = "",
                     text = {""}
                 },
                 j_hanging_chad = {
-                    name = "",
                     text = {
                         "This isn't my {C:attention}first{} time"
                     }
                 },
                 j_caino = {
-                    name = "",
                     text = {
                         "Off with their {C:attention}heads!{}"
                     }
                 },
                 j_greedy_joker = {
-                    name = "",
                     text = {
                         "Hello, can you give me",
                         "your {C:diamonds}Money{}, please?"
                     }
                 },
                 j_lusty_joker = {
-                    name = "",
                     text = {
                         "Hello, can you give me",
                         "your {C:hearts}Love{}, please?"
                     }
                 },
                 j_wrathful_joker = {
-                    name = "",
                     text = {
                         "Hello.",
                         "Go to {C:spades}Hell{}."
                     }
                 },
                 j_gluttenous_joker = {
-                    name = "",
                     text = {
                         "Hello? You can still",
                         "{C:clubs}feed{} me, I'm not done"
                     }
                 },
                 j_jolly = {
-                    name = "",
                     text = {
                         "Also try {C:attention}Cryptid 2!{}",
                         "...Not out yet? Oh..."
                     }
                 },
                 j_lucky_cat = {
-                    name = "",
                     text = {
                         "Meow :3",
                         "Mrow purr {C:green}'Lucky'{} mrau"
                     }
                 },
-                j_square={
-                    name="Square Joker",
-                    text={
+                j_square = {
+                    text = {
                         "Be there, or be {C:attention}square!",
                         "..Right."
                     },
                 },
+                j_8_ball_1 = {
+                    text = {
+                        "It is certain",
+                    }
+                },
+                j_8_ball_2 = {
+                    text = {
+                        "Concentrate and ask again",
+                    }
+                },
+                j_8_ball_3 = {
+                    text = {
+                        "Don't count on it"
+                    }
+                },
+                j_8_ball_4 = {
+                    text = {
+                        "Not exactly"
+                    }
+                },
+                j_bloodstone = {
+                    text = {
+                        "Wait, is that my {C:hearts}Kidney Stone?"
+                    }
+                },
+                j_rough_gem = {
+                    text = {
+                        "Oh, no, THIS is my {C:diamonds}Kidney Stone"
+                    }
+                },
+                j_onyx_agate={
+                    text = {
+                        "Sphinx of {C:clubs}Black Quartz,{}", 
+                        "Judge My Vow"
+                    }
+                },
+                j_arrowhead = {
+                    text = {
+                        "It's like it's some kind of...",
+                        "{C:spades}Terrible Addition{}"
+                    }
+                },
+                j_obelisk = {
+                    text={
+                        "It hums ominously"
+                    },
+                },
+                j_glass = {
+                    text={
+                        "{s:1.5,X:mult,C:white}FRAGILE",
+                        "Please, handle with care"
+                    },
+                },
+                j_vampire = {
+                    text = {
+                        "I {C:mult}suck{} your deck,",
+                        "bleh bleh bleh"
+                    }
+                },
+                j_madness = {
+                    text = {
+                        "{C:mult}No one can escape",
+                        "{C:mult}the miles"
+                    }
+                }
             },
     }
 }
